51f50fe5ff468609a60c7b5114d31e0b77fb3018
victor.onwuhafua <victor.onwuhafua@ISW-GSD-VO>
2022-07-16 12:41:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InversePermutationHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
